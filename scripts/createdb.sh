baf2c0be8a68f811941fb2b9581496daa0063d6f
Marek Dziewit <marek2901@gmail.com>
2019-06-18 16:22:21 +0200

    Hide db cleanup err messages

--- scripts/createdb.sh
+++ scripts/createdb.sh
@@ -5,5 +5,5 @@ if !(which sqlite3 &> /dev/null); then
     exit 1
 fi
 
-rm ../lol.db
+rm ../lol.db &> /dev/null
 sqlite3 ../lol.db "CREATE TABLE CSV_RECORDS(ID INTEGER PRIMARY KEY AUTOINCREMENT, YEAR TEXT NOT NULL, REGION TEXT NOT NULL, CONSUMPTION_TYPE TEXT NOT NULL, CONSUMPTION TEXT NOT NULL);"
