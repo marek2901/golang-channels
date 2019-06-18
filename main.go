88485dfa75fbff784b33154fe084834bdfaed6cf
Marek Dziewit <marek2901@gmail.com>
2019-06-18 14:23:21 +0200

    its lol db

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import channelsfunn "channelsfunn/lib"
 
 func main() {
-	processor := channelsfunn.GetProcessor("electricity-consumption-by-sectors.csv", "elo.db")
+	processor := channelsfunn.GetProcessor("electricity-consumption-by-sectors.csv", "lol.db")
 	err := processor.Process()
 	if err != nil {
 		panic(err)
--- scripts/drytest.sh
+++ scripts/drytest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo SELECT EM ALL
-sqlite3 ../elo.db "SELECT * FROM CSV_RECORDS;"
+sqlite3 ../lol.db "SELECT * FROM CSV_RECORDS;"
 
 echo COUNT
-sqlite3 ../elo.db "SELECT COUNT(*) FROM CSV_RECORDS;"
+sqlite3 ../lol.db "SELECT COUNT(*) FROM CSV_RECORDS;"
 
 echo YAYY
