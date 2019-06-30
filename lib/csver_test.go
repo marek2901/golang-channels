4a8a5fdf1bc38238f2dad4049149cc8638628ba7
Marek Dziewit <marek2901@gmail.com>
2019-06-30 23:51:16 +0200

    Cover passign to insert strategy with tests

--- lib/csver_test.go
+++ lib/csver_test.go
@@ -1,25 +1,55 @@
 package channelsfunn
 
 import (
-	"fmt"
 	"testing"
 )
 
-type dummyInsertStrategy struct{}
+type dummyInsertStrategy struct{
+	customChecks func(csvRecord csvDataModel)
+}
 
 func (dIS dummyInsertStrategy) InsertData(csvRecord csvDataModel) {
-	fmt.Printf("%v", csvRecord)
+	dIS.customChecks(csvRecord)
 }
 
 func TestCsvProcessor_Process(t *testing.T) {
+	insertStrategy := dummyInsertStrategy{
+		customChecks: func(csvRecord csvDataModel) {},
+	}
 	processor := csvProcessor{
-		insertStrategy: dummyInsertStrategy{},
+		insertStrategy: insertStrategy,
 		csvFilePath:    "../electricity-consumption-by-sectors.csv",
 	}
+
+	tearDown := func() { insertStrategy.customChecks = func(csvRecord csvDataModel) {}}
+
 	t.Run("it doesnt fail", func(t *testing.T) {
+		defer tearDown()
 		err := processor.Process()
 		if err != nil {
 			t.Fail()
+		}
+	})
+
+	t.Run("it pass all structs properly", func(t *testing.T) {
+		defer tearDown()
+		err := processor.Process()
+		if err != nil {
+			t.Fail()
+		}
+		insertStrategy.customChecks = func(csvRecord csvDataModel) {
+			if len(csvRecord.Consumption) == 0 {
+				t.Fail()
+			}
+			if len(csvRecord.ConsumptionType) == 0 {
+				t.Fail()
+			}
+			if len(csvRecord.Region) == 0 {
+				t.Fail()
+			}
+			if len(csvRecord.Year) == 0 {
+				t.Fail()
+			}
 		}
 	})
 }
